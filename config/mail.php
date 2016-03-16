82b8c5468497deb54b74c76fed3becc8993502b6
Luis Mayta <luis.mayta@gmail.com>
2016-03-16 09:33:14 -0500

    Reset Password

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@gmail.com', 'name' => 'Luis mayta'],
 
     /*
     |--------------------------------------------------------------------------
